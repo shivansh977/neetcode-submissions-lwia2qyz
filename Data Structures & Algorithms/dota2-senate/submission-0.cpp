e6f72c3223891fd6dfb5eeef50732cc9efe80780
shivansh977 <83871363+shivansh977@users.noreply.github.com>
2026-03-30 23:27:22 +0530

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n=senate.size();
+        vector<bool> isThere(n,true);
+        int i=0;
+        bool flag=true;
+        while(true){
+            for(int k=i+1;k<i+1+n;k++){
+                if(senate[k%n]!=senate[i%n]&&isThere[k%n]){
+                    isThere[k%n]=false;
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag) break;
+            flag=true;
+            for(int k=i+1;k<i+1+n;k++){
+                if(isThere[k%n]){
+                    i=k;
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag) break;
+            flag=true;
+        }
+        bool isRad=true;
+        for(int i=0;i<senate.size();i++){
+            if(isThere[i]&&senate[i]=='R'){
+                return "Radiant";
+            }
+        }
+        return "Dire";
+    }
+};
