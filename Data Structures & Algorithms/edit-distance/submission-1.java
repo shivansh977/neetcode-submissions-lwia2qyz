15cc7ced9ad3814029e0ac2d35e280817498584f
shivansh977 <83871363+shivansh977@users.noreply.github.com>
2026-05-07 16:34:25 +0530

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        StringBuilder s1 = new StringBuilder(word1);
+            StringBuilder s2 = new StringBuilder(word2);
+            int n=s1.length();
+            int m=s2.length();
+            int[][] dp = new int[n+1][m+1];
+            for(int i=1;i<=n;i++){
+                dp[i][0]=dp[i-1][0]+1;
+            }
+            for(int i=1;i<=m;i++){
+                dp[0][i]=dp[0][i-1]+1;
+            }
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    if(s1.charAt(i-1)==s2.charAt(j-1)){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                    }
+                }
+            }
+            return dp[n][m];
+    }
+}
